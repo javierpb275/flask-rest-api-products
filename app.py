0a8becdb55704f73dfe5f93003300840a59b2709
javierpb275 <javierpb275@gmail.com>
2022-08-03 18:23:54 +0200

    delete product

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def add_product():
         "quantity": request.json['quantity'],
     }
     products.append(new_product)
-    return jsonify({"product": new_product})
+    return jsonify({"new_product": new_product})
 
 
 @app.route('/products/<string:name>', methods=['PUT'])
@@ -41,7 +41,16 @@ def update_product(name):
     products_found[0]['name'] = request.json['name']
     products_found[0]['price'] = request.json['price']
     products_found[0]['quantity'] = request.json['quantity']
-    return jsonify({"product": products_found[0]})
+    return jsonify({"updated_product": products_found[0]})
+
+
+@app.route('/products/<string:name>', methods=['DELETE'])
+def delete_product(name):
+    products_found = [product for product in products if product['name'] == name]
+    if len(products_found) <= 0:
+        return jsonify({"message": "Product Not Found"})
+    products.remove(products_found[0])
+    return jsonify({"deleted_product": products_found[0]})
 
 
 if __name__ == '__main__':
