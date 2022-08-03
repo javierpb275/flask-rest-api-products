56f474d31997ff26583bacc2452cd201a6f3d4c8
javierpb275 <javierpb275@gmail.com>
2022-08-03 17:55:37 +0200

    get products and get single product by name

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from products import products
 
 app = Flask(__name__)
@@ -6,7 +6,20 @@ app = Flask(__name__)
 
 @app.route('/ping')
 def ping():
-    return 'pong'
+    return jsonify({"message": "pong"})
+
+
+@app.route('/products')
+def get_products():
+    return jsonify({"products": products})
+
+
+@app.route('/products/<string:name>')
+def get_product(name):
+    products_found = [product for product in products if product['name'] == name]
+    if len(products_found) > 0:
+        return jsonify({"product": products_found[0]})
+    return jsonify({"message": "Product Not Found"})
 
 
 if __name__ == '__main__':
